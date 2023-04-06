45b5c24b72cd1b93695de882c2ac7418d76d756c
vyastreb <profywld@gmail.com>
2023-04-06 18:29:57 +0200

    updated structure

--- Hierarchie/Cluster/tools.py
+++
@@ -1,5 +0,0 @@
-
-import numpy as np 
-
-def Is(S) :
-    return(np.argmax(np.max(S, axis=0) - np.min(S, axis=0)))
--- Hierarchie/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__all__ = ['Cluster']
-
-# from .FastBem import FastBem
-# from .FastBem_integ import Bem_integ
-
